5c4e93930ae275980acab10ae27200708b3c6462
Holly Gong <gongh@google.com>
2024-01-23 18:35:12 +1100

    remove files

--- result/npm/script.sh
+++
@@ -1,6 +0,0 @@
-# target vuln module: static-server
-docker build -t docker-scanning-test-js ../docker/npm/
-trivy image docker-scanning-test-js
-grype docker-scanning-test-js
-SNYK_TOKEN=$token snyk container test docker-scanning-test-js
-docker scout quickview docker-scanning-test-js
